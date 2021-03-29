e5fb647c0cd5c90cffe2e180e2aa3f19ceaeb918
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-03-29 15:57:15 -0700

    Moved external libraries to library/extern
    Note: This branch is based on i18n-xml, so a PR for this branch will not come until the changes from i18n-xml are merged

--- library/borealis.mk
+++ library/borealis.mk
@@ -9,27 +9,27 @@ SOURCES		:=	$(SOURCES) \
 				$(current_dir)/lib/core \
 				$(current_dir)/lib/views \
 				$(current_dir)/lib/platforms/switch \
-				$(current_dir)/lib/extern/glad \
-				$(current_dir)/lib/extern/nanovg-deko3d/source \
-				$(current_dir)/lib/extern/nanovg-deko3d/source/framework \
-				$(current_dir)/lib/extern/nanovg-deko3d/shaders \
-				$(current_dir)/lib/extern/libretro-common/compat \
-				$(current_dir)/lib/extern/libretro-common/encodings \
-				$(current_dir)/lib/extern/libretro-common/features \
-				$(current_dir)/lib/extern/nxfmtwrapper \
-				$(current_dir)/lib/extern/yoga/src/yoga/event \
-				$(current_dir)/lib/extern/yoga/src/yoga \
-				$(current_dir)/lib/extern/tinyxml2/ \
-				$(addprefix $(current_dir)/lib/extern/switch-libpulsar/, $(PLSR_SOURCES))
+				$(current_dir)/extern/glad \
+				$(current_dir)/extern/nanovg-deko3d/source \
+				$(current_dir)/extern/nanovg-deko3d/source/framework \
+				$(current_dir)/extern/nanovg-deko3d/shaders \
+				$(current_dir)/extern/libretro-common/compat \
+				$(current_dir)/extern/libretro-common/encodings \
+				$(current_dir)/extern/libretro-common/features \
+				$(current_dir)/extern/nxfmtwrapper \
+				$(current_dir)/extern/yoga/src/yoga/event \
+				$(current_dir)/extern/yoga/src/yoga \
+				$(current_dir)/extern/tinyxml2/ \
+				$(addprefix $(current_dir)/extern/switch-libpulsar/, $(PLSR_SOURCES))
 
 INCLUDES	:=	$(INCLUDES) \
 				$(current_dir)/include \
-				$(current_dir)/lib/extern/fmt/include \
-				$(current_dir)/lib/extern/yoga/src \
-				$(current_dir)/lib/extern/nanovg-deko3d/include \
-				$(current_dir)/lib/extern/tweeny/include \
-				$(current_dir)/include/borealis/extern \
-				$(current_dir)/include/borealis/extern/tinyxml2 \
-				$(addprefix $(current_dir)/lib/extern/switch-libpulsar/, $(PLSR_INCLUDES))
+				$(current_dir)/extern/fmt/include \
+				$(current_dir)/extern/yoga/src \
+				$(current_dir)/extern/nanovg-deko3d/include \
+				$(current_dir)/extern/tweeny/include \
+				$(current_dir)/extern \
+				$(current_dir)/extern/tinyxml2 \
+				$(addprefix $(current_dir)/extern/switch-libpulsar/, $(PLSR_INCLUDES))
 
 CXXFLAGS := $(CXXFLAGS) -DYG_ENABLE_EVENTS -fdata-sections -DBRLS_RESOURCES="\"romfs:/\""
